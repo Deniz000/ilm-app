446012af618e33a00f8277238f5615b7a7f03782
Deniz000 <guldenizozdemir03@gmail.com>
2024-11-30 14:31:27 +0300

    The Custom Annotation for Email Validation

---
+++ src/main/java/com/ilim/app/controllers/UserDto.java
@@ -0,0 +1,31 @@
+package com.ilim.app.controllers;
+
+import com.ilim.app.business.emailvalidation.ValidEmail;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class UserDto {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+    @ValidEmail
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    // standard getters and setters
+}
